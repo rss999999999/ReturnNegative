bd2c3863d5f62aaa19ddcc382a5c53dd23b8773e
Rodrigo <rss.999999999@gmail.com>
2023-10-01 20:25:21 -0300

    tests update

--- src/Kata.java
+++ src/Kata.java
@@ -1,9 +1,20 @@
+/*
 
+In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+    Examples
+    Kata.makeNegative(1);  // return -1
+    Kata.makeNegative(-5); // return -5
+    Kata.makeNegative(0);  // return 0
+
+*/
+
+import java.lang.Math;
 public class Kata {
 
     public static int makeNegative(final int x) {
 
-        return x*-1;
+        return Math.abs(x) * -1;
 
     }
 
